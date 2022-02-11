86dd62c9f19eb0b67dd40d0d2e9e70e10e823875
Prohor722 <prohor.b722@gmail.com>
2022-02-11 21:18:43 +0600

    bug fixed

--- js/banking_system.js
+++ js/banking_system.js
@@ -1,35 +1,43 @@
-function updateAmmount(inputID, balanceID,add){
+function inputBalance(balance, balanceValue, inputValue,input){
+    balance.innerText = balanceValue + inputValue ;                 //update main balance
+    input.value= '';
+}
+function updateBalance(inputID, balanceID,add){
     const input = document.getElementById(inputID);
     const inputValue = parseFloat(input.value);
 
     const balance = document.getElementById(balanceID);
     const balanceValue = parseFloat(balance.innerText);
 
-    //update balance
-    balance.innerText = balanceValue + inputValue ;
-
-    //reset inputbox
-    input.value= '';
-
-    //update main balance
     const mainBalance = document.getElementById('blance');
     const mainBalanceValue = parseFloat(mainBalance.innerText);
-    if(add== true){
-        mainBalance.innerText = mainBalanceValue + inputValue;
+
+    let error = document.getElementById('error');
+
+    if(inputValue < 0){
+        error.style.display = "block";
     }
     else{
-        mainBalance.innerText = mainBalanceValue - inputValue;
+        if(add== true && inputValue > 0){
+            inputBalance(balance, balanceValue, inputValue,input);
+            mainBalance.innerText = mainBalanceValue + inputValue;          //after diposit main balance
+        }
+        else if(add== false && inputValue <= mainBalanceValue){
+            inputBalance(balance, balanceValue, inputValue,input);
+            mainBalance.innerText = mainBalanceValue - inputValue;          //after withdraw main balance
+        }
+        error.style.display = "none";
     }
 }
 
 //diposit system
 
 document.getElementById('depo-btn').addEventListener('click', function(){
-    const depositAmmount = updateAmmount('deposit','depoTotal',true);
+    const depositAmmount = updateBalance('deposit','depoTotal',true);
 })
 
 //withdraw system
 
 document.getElementById('wdraw-btn').addEventListener('click', function(){
-    const withdrawAmmount = updateAmmount('withdraw','withdrawBalance',false);
+    const withdrawAmmount = updateBalance('withdraw','withdrawBalance',false);
 })
