c4d73072523423ddbc945866c1695f18138ce7bb
Prohor722 <prohor.b722@gmail.com>
2022-02-11 19:45:02 +0600

    deposit and withdraw done.

--- js/banking_system.js
+++ js/banking_system.js
@@ -1,11 +1,25 @@
-function updateAmmount(inputID, blanceID){
+function updateAmmount(inputID, balanceID){
     const input = document.getElementById(inputID);
     const inputValue = parseFloat(input.value);
 
+    const balance = document.getElementById(balanceID);
+    const balanceValue = parseFloat(balance.innerText);
+
+    //update balance
+    balance.innerText = balanceValue + inputValue ;
+
+    //reset inputbox
+    input.value= '';
 }
 
 //diposit system
 
 document.getElementById('depo-btn').addEventListener('click', function(){
     const depositAmmount = updateAmmount('deposit','depoTotal');
+})
+
+//withdraw system
+
+document.getElementById('wdraw-btn').addEventListener('click', function(){
+    const withdrawAmmount = updateAmmount('withdraw','withdrawBalance');
 })
